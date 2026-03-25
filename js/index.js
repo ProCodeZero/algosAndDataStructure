8e8d1272efba4f3ea27e8a2873765fb51b604a66
HollowDS <denis.osipov.2288.02@gmail.com>
2026-03-25 14:44:15 +0300

    init

---
+++ js/index.js
@@ -0,0 +1,67 @@
+function linearSearch(haystack, needle) {
+  let isFound = false;
+  let count = 0;
+  for (let i = 0; i < haystack.length; i++) {
+    count++;
+    if (haystack[i] === needle) {
+      isFound = true;
+      break;
+    }
+  }
+  return isFound;
+}
+
+function binarySearch(haystack, needle) {
+  let count = 0;
+  let high = haystack.length;
+  let low = 0;
+  do {
+    const middle = Math.floor(low + (high - low) / 2);
+    const value = haystack[middle];
+    count++;
+    if (value === needle) {
+      return true;
+    } else if (value > needle) {
+      high = middle;
+    } else {
+      low = middle + 1;
+    }
+  } while (low < high);
+  return false;
+}
+
+function firstBreak(breaks) {
+  const jumpValue = Math.floor(Math.sqrt(breaks.length));
+  let sliceValue = 0;
+  for (let i = 0; i < breaks.length; i += jumpValue) {
+    if (breaks[i] === 1) {
+      sliceValue = i;
+      break;
+    }
+  }
+  for (let i = sliceValue - jumpValue; i < sliceValue; i++) {
+    if (breaks[i] === 1) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+const breaks = [];
+const orderedTestArray = [];
+
+// filling array constants
+for (let i = 0; i < 1000; i++) {
+  if (i < 621) {
+    breaks[i] = 0;
+  } else {
+    breaks[i] = 1;
+  }
+  orderedTestArray[i] = i;
+}
+
+console.log(linearSearch(orderedTestArray, 10000));
+console.log(linearSearch(orderedTestArray, 750));
+console.log(binarySearch(orderedTestArray, 10000));
+console.log(binarySearch(orderedTestArray, 750));
+console.log(firstBreak(breaks));
