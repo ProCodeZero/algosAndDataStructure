65dd906b2d92f9d78a74a39e3bf9ae43d9c5be65
HollowDS <denis.osipov.2288.02@gmail.com>
2026-03-28 15:46:08 +0300

    queue js implementation

--- js/structures.ts
+++ js/structures.ts
@@ -1,3 +1,4 @@
+// Linked List
 interface LinkedList<T> {
   get length(): number;
   insertAt(item: T, index: number): void;
@@ -16,4 +17,53 @@ class MyLinkedList implements LinkedList<T> {
   append(item: T): void {}
   prepend(item: T): void {}
   get(index: number): T | undefined {}
+}
+
+//Queue
+type QNode<T> = {
+  value: T;
+  next?: QNode<T>;
+};
+
+export class Queue<T> {
+  public length: number;
+  private head?: QNode<T>;
+  private tail?: QNode<T>;
+
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
+
+  enqueue(item: T): void {
+    const node = { value: item } as QNode<T>;
+    this.length++;
+    if (!this.tail) {
+      this.tail = this.head = node;
+    }
+    this.tail.next = node;
+    this.tail = node;
+  }
+
+  deque(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
+
+    this.length--;
+
+    const head = this.head;
+    this.head = this.head.next;
+
+    // free
+    head.next = undefined;
+
+    // what about tail?
+
+    return head.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
