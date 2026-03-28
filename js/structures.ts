68115e4a978d28eb35ffcac0899ec34bf2b00e39
HollowDS <denis.osipov.2288.02@gmail.com>
2026-03-28 16:28:19 +0300

    stack implementation

--- js/structures.ts
+++ js/structures.ts
@@ -67,3 +67,53 @@ export class Queue<T> {
     return this.head?.value;
   }
 }
+
+// Stack
+type SNode<T> = {
+  value: T;
+  prev?: SNode<T>;
+};
+
+export class Stack<T> {
+  public length: number;
+  private head?: SNode<T>;
+
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
+
+  push(item: T): void {
+    const node = { value: item } as SNode<T>;
+
+    this.length++;
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    node.prev = this.head;
+    this.head = node;
+  }
+
+  pop(): T | undefined {
+    this.length = Math.max(0, this.length - 1);
+
+    if (this.length === 0) {
+      const head = this.head;
+      this.head = undefined;
+      return head?.value;
+    }
+
+    const head = this.head as SNode<T>;
+    this.head = head.prev;
+
+    // free
+
+    return head.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+  }
+}
